73c824d016ae953e9b980c223372b9a7bbe04d1a
lxstik <egorrfal32@gmail.com>
2024-10-23 16:08:15 +0200

    caja-fuerte-ver1

---
+++ practicas/cajaFuerte/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Sombrero Seleccionador de Hogwarts</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+<h1 class="text-center mb-4">Benvinguts a Hogwarts</h1>
+AQUI VA EL FORMULARI
+</div>
+</body>
+</html>
+
+Aquí us deixo l'array multidimensional ja construit.... més fàcil impossible
+
+// Array multidimensional amb la informació de cada casa
+$casas_info = [
+"Gryffindor" => [
+"background_color" => "#740001",
+"text_color" => "#FFD700",
+"welcome_message" => "Coratge, valor i determinació. Benvingut a Gryffindor!",
+"message_background" => "#D3A625",
+"image" => ""
+],
+"Hufflepuff" => [
+"background_color" => "#FFDB00",
+"text_color" => "#60605B",
+"welcome_message" => "Lleialtat, paciència i treball dur. Benvingut a Hufflepuff!",
+"message_background" => "#EEE117",
+"image" => ""
+],
+"Ravenclaw" => [
+"background_color" => "#0E1A40",
+"text_color" => "#946B2D",
+"welcome_message" => "Intel·ligència, creativitat i saviesa. Benvingut a Ravenclaw!",
+"message_background" => "#5D5D5D",
+"image" => ""
+],
+"Slytherin" => [
+"background_color" => "#1A472A",
+"text_color" => "#AAAAAA",
+"welcome_message" => "Ambició, astúcia i lideratge. Benvingut a Slytherin!",
+"message_background" => "#5D5D5D",
+"image" => ""
+]
+];
